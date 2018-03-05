9fa0f8887cdb6675bb596e0fc514bce2bb71d720
USER <USER@10.150.131.44>
2018-03-05 09:53:30 -0500

    allo le gros cave

---
+++ src/learn_slick2d/StateGame.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package learn_slick2d;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+/**
+ *
+ * @author USER
+ */
+public class StateGame extends StateBasedGame {
+
+    public static void main(String[] args) throws SlickException {
+        new AppGameContainer(new StateGame(), 800, 600, false).start();
+    }
+
+    public StateGame() {
+        super("Quest inside the human's body");
+    }
+
+    @Override
+    public void initStatesList(GameContainer container) throws SlickException {
+        addState(new MainScreenGameState());
+        addState(new MapGameState());
+         addState(new OptionsMenuState());
+         addState(new IntroState());
+    }
+
+}
